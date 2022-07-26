817ab8dc8d6ab2d134738af5b7dd6c09f4959f30
Isaac Gonzalez <u1337x@gmail.com>
2022-07-26 11:56:55 -0400

    Update

---
+++ controllers/budget.controller.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const budget = express.Router();
+const purchaseArray = require("../models/budget");
+
+// Index
+budget.get("/", (req, res) => {
+  res.json(purchaseArray);
+});
+// Show
+budget.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (purchaseArray[id]) {
+    res.send(purchaseArray[id]);
+  } else {
+    res.redirect("*");
+  }
+});
+// Create
+budget.post("/", (req, res) => {
+  purchaseArray.push(req.body);
+  res.json(purchaseArray[purchaseArray.length - 1]);
+});
+// Delete
+budget.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  purchaseArray.splice(id, 1);
+  res.send(budgetData);
+});
+// Update
+budget.put("/:id", (req, res) => {});
+
+module.exports = budget;
---
+++ models/budget.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    item_name: "BLACK COMP DNA ATLETA",
+    amount: 194.0,
+    date: "7/25",
+    from: "Origin",
+    category: "MMA",
+  },
+  {
+    item_name: "Baseline Ranked Long Sleeve Rashguard White",
+    amount: 45.0,
+    date: "7/25",
+    from: "Fuji",
+    category: "MMA",
+  },
+  {
+    item_name: "Fuji BJJ Belt - Original Brazilian Jiu Jitsu Rank Belt",
+    amount: 22.95,
+    date: "7/25",
+    from: "Amazon",
+    category: "MMA",
+  },
+];
